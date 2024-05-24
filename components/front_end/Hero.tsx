6071db2a76613ad1a662fdcdf278fc62b0e7387e
Indra <hiimindra12345@gmail.com>
2024-05-24 20:31:23 +0530

    safety-commit-1

--- components/front_end/Hero.tsx
+++ components/front_end/Hero.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import hero from '@/public/hero.png'
 import Image from 'next/image'
 import { Button } from '../ui/button'
+import location from '@/public/location.svg'
+import arrow_down from '@/public/arrow_down.svg'
+import {Jua,Manrope} from '@next/font/google'
+
+const jua = Jua({
+  weight: "400",
+  subsets: ['latin'],
+});
+
+const manrope = Manrope({
+  weight: "400",
+  subsets: ['latin'],
+});
+
+
 const Hero = () => {
   return (
     < >
-      <div 
-        className='flex flex-row justify-center items-center w-full pt-16'
-      >
+      <div className='flex flex-row justify-center items-center w-full pt-16'>
         <Image
             src={hero} 
             alt='hero'
@@ -15,25 +28,33 @@ const Hero = () => {
         />
       </div>
 
-      <div className=' flex flex-col justify-center items-center pt-8 font-extrabold text-4xl bg-grey w-full h-64 '>
-        <div className='group flex flex-col'>
-          
-          <div className="flex">
-            <span className='text-violetBlue'> Where is </span>
+      <div className="flex flex-col justify-start items-center pt-8 font-extrabold text-5xl bg-grey w-full h-96" >
 
-            <span className='px-5 transform rotate-45 text-violetBlue text-7xl group-hover:text-yellowDark  relative bottom-5 ' style={{transform: 'rotate(-45deg) '}}> ? </span> 
+        <div className="group flex flex-col  text-violetBlue">
+          
+          <div className="flex ">
+            <span> Where is </span>
+            <span className=' transform rotate-45 font-medium text-7xl group-hover:text-yellowDark relative bottom-5  ' style={{transform: 'rotate(-35deg) '}}> ? </span> 
           </div>
 
           <div className='flex relative bottom-10'>
-            <span className=' text-violetBlue'> my </span>
-            <span className='text-violetBlue text-7xl group-hover:text-yellowDark'> BUS </span>
+            <span> my </span>
+            <span className=' text-8xl pt-1 font-normal group-hover:text-yellowDark'> BUS </span>
           </div>
 
-        </div>   
+        </div>    
 
-        <Button className='' >
-          <span className='' >Track Buses</span>
-        </Button>
+        <div className='flex flex-col justify-center items-center relative bottom-7 ' >
+          <Button variant="outline" className=" bg-yellowDark  w-80  gap-1 shadow-lg border-neutral-950 h-[3rem]" >
+            <Image src={location} alt='location_marker' className='size-6 pt-1 mb-0.5' />
+            <span className='text-2xl font-semibold' >Track Buses</span>
+          </Button>
+
+          <div className='flex flex-row gap-2'>
+            <Image src={arrow_down} alt="arrowdown" className='object-contain size-4 mt-2 ' />
+            <span className='text-xl text-center font-semibold underline ' >Know More</span>
+          </div>
+        </div>
 
       </div>
 
