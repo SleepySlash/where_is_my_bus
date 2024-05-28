49514796c780f2299b32935eb819537233114ff6
Indra <hiimindra12345@gmail.com>
2024-05-28 22:02:23 +0530

    Half of  the Home Page is Done 2 more, sections are left

--- components/front_end/Home.tsx
+++ components/front_end/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Jua,Manrope} from "next/font/google"
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import logo from '@/public/logo.svg'
 
 const jua = Jua({
   weight: "400",
@@ -16,15 +18,18 @@ const manrope = Manrope({
 const Home = () => {
   return (
     <>
-       <div className='flex flex-col justify-start items-center h-[25.4375rem] w-full bg-yellowLight px-5'>
-        <span className={cn("font-semibold text-textGrey text-[1.4rem] pr-5 pt-[1.1rem]", jua.className)}>Welcome to</span>
-        <span className={cn("font-extrabold text-violetBlue text-4xl pr-3 pb-10 tracking-tight", jua.className)}>Where is my bus?</span>
-
-        <div className='flex flex-col items-center justify-between p-4'>
-          <p className='max-w-full break-words text-justify text-sm relative bottom-7 '>
-            Your ultimate companion for hassle-free college commutes! Designed with students in mind, our progressive web application provides real-time tracking for college buses, ensuring you never miss a ride. Join us and experience the convenience of knowing exactly where your bus is, every step of the way!
-          </p>
+       <div className='flex flex-col justify-start items-center h-[23.1em] w-full bg-yellowLight px-2'>
+        <span className={cn("font-semibold text-textGrey text-[1.375rem] pr-2 mt-[1.25rem]", jua.className)}>Welcome to</span>
+        <div className='flex flex-col justify-center text-justify ' >
+          <span className={cn("font-extrabold text-violetBlue text-4xl pr-[-0.3rem] mt-[-0.3rem]", jua.className)}>Where is my bus?</span>
         </div>
+
+        <div className='flex flex-col items-center justify-center p-4 text-justify '>
+          <p className='max-w-full tracking-[-0.035em] text-justify text-[0.83rem] mt-[0.85rem] leading-tight relative'>Your ultimate companion for hassle-free college commutes! Designed with students in mind, our progressive web application provides real-time tracking for college buses, ensuring you never miss a ride. Join us and experience the convenience of knowing exactly where your bus is, every step of the way!</p>
+        </div>
+
+        <Image src={logo} alt='logo' className='mr-1 size-[9.5rem]'/>
+
     </div>
         
       
