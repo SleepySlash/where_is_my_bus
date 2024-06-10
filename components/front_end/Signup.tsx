d5ec1a7811c990fc95b4b7c6de5270f065375ca8
Indra <hiimindra12345@gmail.com>
2024-06-10 20:48:29 +0530

    signup working

--- components/front_end/Signup.tsx
+++ components/front_end/Signup.tsx
@@ -1,11 +1,10 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {cn, colleges} from "@/lib/utils";
+import { cn, colleges } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Selectcollege } from "./Selectcolleg";
-import {Kumbh_Sans } from 'next/font/google';
-
+import { Kumbh_Sans } from 'next/font/google';
 
 const kumbh_sans_darker = Kumbh_Sans({
   weight: '700',
@@ -13,34 +12,34 @@ const kumbh_sans_darker = Kumbh_Sans({
 });
 const kumbh_sans_lighter = Kumbh_Sans({
   weight: '400',
-  subsets: ['latin'],
+  subsets: ['latin'], 
 });
 
 export function Signup() {
   return (
-    <div className={cn("grid w-full max-w-sm items-center gap-[1.25rem] relative")}>
+    <div className={cn("grid w-full max-w-sm items-center gap-[1.25rem] relative min-h-screen")}>
       <div className="right-0 flex flex-col justify-center gap-1">
-        <Label htmlFor="collegeName" className={cn(" text-[#3D408A] font-semibold pl-2",kumbh_sans_darker.className)}>
+        <Label htmlFor="collegeName" className={cn(" text-[#3D408A] font-semibold pl-2", kumbh_sans_darker.className)}>
           College Name
         </Label>
         <Selectcollege />
       </div>
-      <div className={cn("right-0 flex flex-col gap-1 justify-center",kumbh_sans_darker.className)}>
+      <div className={cn("right-0 flex flex-col gap-1 justify-center", kumbh_sans_darker.className)}>
         <Label htmlFor="mobilenumber" className=" text-[#3D408A] font-semibold pl-2">
           Mobile Number
-        </Label>  
+        </Label>
         <Input
-          type="number" 
+          type="number"
           id="email"
           placeholder="Enter Mobile number"
-          className="focus:outline-[#3D408A] "
+          className="focus:outline-[#3D408A]"
         />
-        <div className="flex flex-row items-end justify-end" >
+        <div className="flex flex-row items-end justify-end">
           <p className="text-sm font-bold underline">Send OTP</p>
         </div>
       </div>
-      
-      <div className={cn("flex flex-col gap-1",kumbh_sans_darker.className)}>
+
+      <div className={cn("flex flex-col gap-1", kumbh_sans_darker.className)}>
         <Label htmlFor="otp" className=" text-[#3D408A] font-semibold pl-2">
           OTP
         </Label>
@@ -52,25 +51,25 @@ export function Signup() {
         />
       </div>
 
-      <div className={cn("flex flex-col top-32 relative ",kumbh_sans_darker.className)}>
-        
+      <div className={cn("flex flex-col relative top-32", kumbh_sans_darker.className)}>
         <Button
           variant={"blueg"}
           size={"blueg"}
-          className={cn("rounded-xl h-[3rem] shadow-2xl text-[1rem] ")}
+          className={cn("rounded-xl h-[3rem] shadow-2xl text-[1rem]")}
           onClick={() => console.log("Clicked")}
         >
           Sign Up
         </Button>
-        
-        <div className="px-4 pt-2 text-violetBlue relative items-center">
-          <p className="text-sm font-semibold">
-            Are you an Administrator? 
-            <span className="text-[#3D408A] underline tracking-wide">
-              Login <Link href="/admin">Here</Link>
-            </span>
-          </p>
-        </div>
+      </div>
+
+      {/* Adjusted div to be displayed 8 rem above the bottom */}
+      <div className="absolute bottom-32 left-1/2 transform -translate-x-1/2 text-violetBlue items-center">
+        <p className="text-sm font-semibold">
+          Are you an Administrator?
+          <span className="text-[#3D408A] underline tracking-wide">
+            Login <Link href="/admin">Here</Link>
+          </span>
+        </p>
       </div>
     </div>
   );
