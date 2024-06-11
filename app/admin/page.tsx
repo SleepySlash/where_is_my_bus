dc4809d70688b4b9954075f8863ca4962ba7702e
Indra <hiimindra12345@gmail.com>
2024-06-11 21:53:34 +0530

    Admin Sign Up page is added

---
+++ app/admin/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import Navbar from "@/components/front_end/Navbar";
+import { Signup } from "@/components/front_end/Signup";
+import {Kumbh_Sans } from 'next/font/google';
+import { cn } from "@/lib/utils";
+import { Admin_Signup } from "@/components/front_end/Admin_Signup";
+
+const kumbh_sans_darker = Kumbh_Sans({
+  weight: '700',
+  subsets: ['latin'],
+});
+
+export default function Home() {
+  return (
+    <div className="relative flex flex-col min-h-screen md:hidden">
+      <Navbar />
+      <div className="flex flex-col items-start mt-[4rem] p-[1.5rem] ">
+        <p className={cn(" font-semibold text-[#919191] text-xl",kumbh_sans_darker.className)}>
+          Adminstator
+        </p>
+        <p className={cn("text-[#3D408A] text-5xl font-semibold",kumbh_sans_darker.className)}>Sign Up</p>
+
+        <div className={cn("p-2 pt-[2rem] w-full flex items-center justify-center z-20")}>
+          <Admin_Signup />
+        </div>
+        
+        <div className="absolute bottom-0 left-0 right-0 z-0">
+          <Image
+            src={"/Ellipse.svg"}
+            alt={"Ellipse"}
+            width={767}
+            height={10}
+          />
+        </div>
+        {/* <button type="button" className="h-auto cursor-pointer" onClick={()=>console.log('CLicked')}>sendotp</button> */}
+      </div>
+    </div>
+  );
+}
---
+++ components/front_end/Admin_Signup.tsx
@@ -0,0 +1,77 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {cn, colleges} from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Selectcollege } from "./Selectcolleg";
+import {Kumbh_Sans } from 'next/font/google';
+
+
+const kumbh_sans_darker = Kumbh_Sans({
+  weight: '700',
+  subsets: ['latin'],
+});
+const kumbh_sans_lighter = Kumbh_Sans({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export function Admin_Signup() {
+  return (
+    <div className={cn("grid w-full max-w-sm items-center gap-[1.25rem] relative")}>
+      <div className="right-0 flex flex-col justify-center gap-1">
+        <Label htmlFor="collegeName" className={cn(" text-[#3D408A] font-semibold pl-2",kumbh_sans_darker.className)}>
+          College Name
+        </Label>
+        <Selectcollege />
+      </div>
+      <div className={cn("right-0 flex flex-col gap-1 justify-center",kumbh_sans_darker.className)}>
+        <Label htmlFor="mobilenumber" className=" text-[#3D408A] font-semibold pl-2">
+          Mobile Number
+        </Label>  
+        <Input
+          type="number" 
+          id="email"
+          placeholder="Enter Mobile number"
+          className="focus:outline-[#3D408A] "
+        />
+        <div className="flex flex-row items-end justify-end" >
+          <p className="text-sm font-bold underline">Send OTP</p>
+        </div>
+      </div>
+      
+      <div className={cn("flex flex-col gap-1",kumbh_sans_darker.className)}>
+        <Label htmlFor="otp" className=" text-[#3D408A] font-semibold pl-2">
+          OTP
+        </Label>
+        <Input
+          type="number"
+          id="otp"
+          placeholder="Enter OTP"
+          className="focus:outline-[#3D408A]"
+        />
+      </div>
+
+      <div className={cn("flex flex-col top-32 relative ",kumbh_sans_darker.className)}>
+        
+        <Button
+          variant={"blueg"}
+          size={"blueg"}
+          className={cn("rounded-xl h-[3rem] shadow-2xl text-[1rem] ")}
+          onClick={() => console.log("Clicked")}
+        >
+          Sign Up
+        </Button>
+        
+        <div className="px-4 pt-2 text-violetBlue relative items-center">
+          <p className="text-sm font-semibold">
+            Are you a Student ? 
+            <span className="text-[#3D408A] underline tracking-wide">
+              Login <Link href="/signup">Here</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
