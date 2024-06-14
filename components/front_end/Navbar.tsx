befdbcfa8bbf755efaf3c9008ac4aa9e694c820e
Abhiram <vemulaabhiram2004@gmail.com>
2024-06-14 14:41:02 +0530

    Front End: Added Sidebar

--- components/front_end/Navbar.tsx
+++ components/front_end/Navbar.tsx
@@ -1,50 +1,91 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from '@/public/logo.svg'
-import settings from '@/public/settings.svg'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import logo from "@/public/logo.svg";
+import settings from "@/public/settings.svg";
+
+import { Button } from "../ui/button";
+import Item from "../ui/Sidebar/Item";
+import Social from "../ui/Sidebar/Social";
 
 interface NavbarProps {
-    Route?: React.ReactNode,
-    BusDetails?: React.ReactNode,
+  Route?: React.ReactNode;
+  BusDetails?: React.ReactNode;
 }
-const Navbar = ({Route,BusDetails}:NavbarProps) => {
+const Navbar = ({ Route, BusDetails }: NavbarProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-        <div className='fixed top-[-2px] left-0 w-full flex justify-between pb-4 items-strech shadow-xl bg-white z-50'>
-            <Link
-                href="/"
-                className=''
-                passHref
-            >
-                <Image
-                    src={logo}
-                    alt='logo'
-                    className='cursor-pointer ml-6 h-8 w-8 mt-4 '
-                />
-            </Link>
+      <div className="sticky top-0 w-full min-h-[7vh] flex justify-between items-center bg-white py-2 px-5 shadow-md">
+        <Link href="/" className="">
+          <Image src={logo} alt="logo" className="cursor-pointer h-8 w-8" />
+        </Link>
 
-            {/* The Route Details will be sent as a React Component*/}
-            <span>
-                {Route}
-            </span>
-    
-            <Link
-                href="/settings"
-                className=''
-            >
-                <Image
-                    src={settings}
-                    alt='settings'
-                    className='mr-5 pt-2 h-8 w-8 mt-4 '
-                />
-            </Link>
+        {/* The Route Details will be sent as a React Component*/}
+        <span>{Route}</span>
 
+        <Button
+          onClick={() => setOpen(true)}
+          className="bg-transparent shadow-none hover:bg-transparent"
+        >
+          <Image src={settings} alt="settings" className="h-8 w-8" />
+        </Button>
+      </div>
+
+      {/* When there Route Details sent they have to be rendered as bar below the navbar  */}
+      <div
+        className={`fixed top-0 right-0 min-h-[101vh] p-5 pr-10 z-10 bg-white min-w-[60vw] flex justify-between flex-col transition-all duration-200 ${
+          !open ? "translate-x-[100%]" : ""
+        }`}
+      >
+        <Button
+          onClick={() => setOpen(false)}
+          className="absolute top-1 right-3 cursor-pointer bg-transparent shadow-none hover:bg-transparent"
+        >
+          <Image
+            src={"/close.svg"}
+            className="h-4 w-4 "
+            alt="close"
+            height={10}
+            width={10}
+          />
+        </Button>
+
+        <div>
+          <div className="relative">
+            <Image
+              src={"/main_logo.svg"}
+              alt="Main"
+              height={1}
+              width={1}
+              className="w-[7rem] aspect-square "
+            />
+          </div>
+          <Item name="Profile" icon="/user_blue.svg" linkto="/" />
+          <Item name="Contact Us" icon="/contact.svg" linkto="/" />
+          <Item name="Install App" icon="/heart.svg" linkto="/" />
         </div>
 
-        {/* When there Route Details sent they have to be rendered as bar below the navbar  */}
-    </>    
-  )
-}
+        <div className="flex flex-col gap-1">
+          <p className="">Change Language</p>
+          <div className="border p-3 rounded-lg border-violetBlue">
+            <p>English(IND)</p>
+          </div>
+          <p>Share App</p>
+          <p>Suggest a feature</p>
+          <p className="text-violetBlue">Follow Us</p>
+          <div className="flex gap-3">
+            <Social source="/" alt="insta" icon="/insta.svg" />
+            <Social source="/" alt="twitter" icon="/twitter.svg" />
+            <Social source="/" alt="browser" icon="/browser.svg" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ components/ui/Sidebar/Social.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Social = ({
+  source,
+  icon,
+  alt,
+}: {
+  source: string;
+  icon: string;
+  alt: string;
+}) => {
+  return (
+    <Link href={source}>
+      <Image src={icon} alt={alt} height={10} width={10} className="h-5 w-5" />
+    </Link>
+  );
+};
+
+export default Social;
