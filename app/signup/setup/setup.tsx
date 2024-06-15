0b02fd7444d5312bcc823aa138830972b729dc6c
dinesh-p0 <dineshkrishna0456@gmail.com>
2024-06-15 12:01:38 +0530

    selectbusroutee

--- app/signup/setup/setup.tsx
+++ app/signup/setup/setup.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import Navbar from "@/components/front_end/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
 import React from "react";
 
 const setup = () => {
