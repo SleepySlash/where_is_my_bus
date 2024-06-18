0eda0095902acdaf982898dabbb8a45b9237a5d3
Abhiram <vemulaabhiram2004@gmail.com>
2024-06-18 09:51:19 +0530

    Front End: Bug fixing - About Us heading

--- components/front_end/Home/About.tsx
+++ components/front_end/Home/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="flex flex-col items-center w-full px-7 pt-5 bg-yellowLight">
       <h2 className=" text-textGrey text-2xl font-jua">Welcome to</h2>
 
-      <h1 className="text-violetBlue text-4xl font-jua leading-7">
+      <h1 className="text-violetBlue text-4xl font-jua leading-7 text-center">
         Where is my bus?
       </h1>
 
