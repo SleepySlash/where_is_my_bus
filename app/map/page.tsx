8fad780ff16ac826f9d8956cd7fae71cad46d6d3
Indra <hiimindra12345@gmail.com>
2024-06-21 12:43:35 +0530

    working on leaflet map

---
+++ app/map/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import dynamic from "next/dynamic";
+import Navbar from "@/components/front_end/Navbar";
+
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
+
+// Create a custom marker icon
+const customIcon = new L.Icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+const Page = () => {
+  return (
+    <div className="flex flex-col justify-center">
+      <Navbar Route="Route 1" />
+
+      <div className="w-full h-full">
+        <MapContainer
+          center={[30.443405163585357, -88.53703602002447]}
+          zoom={13}
+          style={{ height: "100vh" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={[30.443405163585357, -88.53703602002447]}
+            icon={customIcon}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
