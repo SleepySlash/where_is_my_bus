6d061d03c2b5b69e4502d630e7103057c6d5ba7b
Indra <hiimindra12345@gmail.com>
2024-06-24 10:46:26 +0530

    adding evening stop field

--- app/signup/setup/page.tsx
+++ app/signup/setup/page.tsx
@@ -29,15 +29,19 @@ import {
 } from "@/components/ui/select";
 import Link from "next/link";
 import { useState } from "react";
+
 const montserrat = Montserrat({
   weight: "500",
   subsets: ["latin"],
 });
+
 const montserrat_lighter = Kumbh_Sans({
   weight: "400",
   subsets: ["latin"],
 });
+
 const array = Array.from({ length: 24 });
+
 const Setup = () => {
   const form = useForm<z.infer<typeof profileSchema>>({
     resolver: zodResolver(profileSchema),
@@ -50,22 +54,37 @@ const Setup = () => {
       gender: "",
       routeno: "",
       area: "",
-      // coordinate: "",
+      eveningCoordinate: "", // Add default value for eveningCoordinate
     },
   });
+
   function onSubmit(values: z.infer<typeof profileSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log("Hello");
     console.log(values);
   }
+
   const [user, setUser] = useState(false);
-  // const [busno,setBusno]=useState(1);
+  const [eveningStop, setEveningStop] = useState(false);
+
+  const scrollToEveningStop = (event: any) => {
+    event.preventDefault();
+    setEveningStop(true);
+    document.getElementById("end")?.scrollIntoView({ behavior: "instant" });
+  };
+
+  const removeEveningStop = (event: any) => {
+    event.preventDefault();
+    setEveningStop(false);
+    form.resetField("eveningCoordinate"); // Reset the eveningCoordinate field
+  };
+
   return (
-    <div className="relative flex flex-col md:hidden font-kumbh_sans">
+    <div className="relative flex flex-col font-kumbh_sans">
       <Navbar />
 
-      <div className="flex flex-col justify-between min-h-[93vh] items-center font-bold  ">
+      <div className="flex flex-col justify-between min-h-[93vh] items-center font-bold">
         <div className="w-full flex-1 p-[1.5rem]">
           <p className={cn(" font-bold text-[#919191] text-xl")}>
             Account Details
@@ -77,18 +96,18 @@ const Setup = () => {
 
           <br />
 
-          <div className="pt-[2rem] z-20 ">
+          <div className="pt-[2rem] z-20">
             <Form {...form}>
               <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className=" flex-grow w-full flex flex-col items-center justify-center gap-1 "
+                className="flex-grow w-full flex flex-col items-center justify-center gap-1"
               >
                 <FormField
                   control={form.control}
                   name="firstname"
                   render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
+                      <FormLabel className="text-violetBlue font-semibold pl-3">
                         First Name{" "}
                         <span className="text-red-600 align-super">*</span>
                       </FormLabel>
@@ -104,7 +123,7 @@ const Setup = () => {
                   name="lastname"
                   render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
+                      <FormLabel className="text-violetBlue font-semibold pl-3">
                         Last Name{" "}
                         <span className="text-red-600 align-super">*</span>
                       </FormLabel>
@@ -120,7 +139,7 @@ const Setup = () => {
                   name="rollno"
                   render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
+                      <FormLabel className="text-violetBlue font-semibold pl-3">
                         Roll Number{" "}
                       </FormLabel>
                       <FormControl>
@@ -131,29 +150,13 @@ const Setup = () => {
                   )}
                 />
 
-                {/* <FormField
-                  control={form.control}
-                  name="dob"
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
-                        Dob{" "}
-                      </FormLabel>
-                      <FormControl>
-                        <Input placeholder="yyyy-mm-dd" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                /> */}
-
-                <div className="flex justify-between gap-4 w-full ">
+                <div className="flex justify-between gap-4 w-full">
                   <FormField
                     control={form.control}
                     name="dob"
                     render={({ field }) => (
                       <FormItem className="w-full">
-                        <FormLabel className="text-violetBlue font-semibold  pl-3">
+                        <FormLabel className="text-violetBlue font-semibold pl-3">
                           Dob
                         </FormLabel>
                         <FormControl>
@@ -167,13 +170,11 @@ const Setup = () => {
                     control={form.control}
                     name="gender"
                     render={({ field }) => (
-                      <FormItem className=" w-full">
+                      <FormItem className="w-full">
                         <div className="gap-2 w-full">
                           <FormLabel
                             htmlFor="collegeName"
-                            className={cn(
-                              " text-violetBlue font-semibold pl-3"
-                            )}
+                            className={cn("text-violetBlue font-semibold pl-3")}
                           >
                             Gender
                           </FormLabel>
@@ -187,7 +188,7 @@ const Setup = () => {
                                   <SelectValue
                                     placeholder="select gender"
                                     className={cn(
-                                      "focus:outline-[#3D408A] outline-[#3D408A] "
+                                      "focus:outline-[#3D408A] outline-[#3D408A]"
                                     )}
                                     {...field}
                                   />
@@ -221,11 +222,11 @@ const Setup = () => {
                   control={form.control}
                   name="routeno"
                   render={({ field }) => (
-                    <FormItem className=" w-full">
+                    <FormItem className="w-full">
                       <div className="gap-2 w-full">
                         <FormLabel
                           htmlFor="collegeName"
-                          className={cn(" text-violetBlue font-semibold pl-3")}
+                          className={cn("text-violetBlue font-semibold pl-3")}
                         >
                           Route Number
                         </FormLabel>
@@ -239,7 +240,7 @@ const Setup = () => {
                                 <SelectValue
                                   placeholder="Select Route"
                                   className={cn(
-                                    "focus:outline-[#3D408A] outline-[#3D408A] "
+                                    "focus:outline-[#3D408A] outline-[#3D408A]"
                                   )}
                                   {...field}
                                 />
@@ -268,95 +269,85 @@ const Setup = () => {
                     </FormItem>
                   )}
                 />
-                {/* <div className="w-full flex flex-col">
-                  <Label
-                    htmlFor="Bus Route"
-                    className={cn(" text-violetBlue font-semibold pl-3")}
-                  >
-                    Bus Route
-                  </Label>
-                  <Select>
-                    <SelectTrigger
-                      className={cn(
-                        "outline-[#3D408A] focus:outline-[#3D408A] w-full"
-                      )}
-                    >
-                      <SelectValue
-                        placeholder="Select Route"
-                        className={cn(
-                          "focus:outline-[#3D408A] outline-[#3D408A]",
-                          montserrat_lighter.className
-                        )}
-                      />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        {array.map((_, ind) => (
-                          <SelectItem
-                            key={ind}
-                            value={`${ind + 1}`}
-                            className={cn(
-                              "focus:outline-none",
-                              montserrat.className
-                            )}
-                          >
-                            Route No {ind + 1}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div> */}
-                <FormField
-                  control={form.control}
-                  name="area"
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
-                        Area / Landmark Name{" "}
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="enter area / landmark name"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
                 <FormField
                   control={form.control}
                   name="coordinate"
                   render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel className="text-violetBlue font-semibold  pl-3">
+                      <FormLabel className="text-violetBlue font-semibold pl-3">
                         Enter coordinate
                       </FormLabel>
                       <FormControl>
                         <Input placeholder="0.000.1111" {...field} />
                       </FormControl>
+                      <div
+                        className={`w-full text-sm text-end ${
+                          !eveningStop ? "" : "hidden"
+                        }`}
+                      >
+                        <span className="text-[#9CA3AF]">
+                          Is your Evening Stop different?{" "}
+                          <a
+                            href="#end"
+                            className="bg-transparent text-violetBlue font-bold underline"
+                            onClick={scrollToEveningStop}
+                          >
+                            Click me
+                          </a>
+                        </span>
+                      </div>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-
-                <Button
-                  type="submit"
-                  variant={"blueg"}
-                  size={"blueg"}
-                  className={cn(
-                    "rounded-xl h-[3rem] shadow-2xl text-[1rem] w-full"
+                <FormField
+                  control={form.control}
+                  name="eveningCoordinate"
+                  render={({ field }) => (
+                    <FormItem
+                      className={`w-full ${eveningStop ? "" : "hidden"}`}
+                      id="end"
+                    >
+                      <FormLabel className="text-violetBlue font-semibold pl-3">
+                        Evening Coordinate
+                      </FormLabel>
+                      <FormControl>
+                        <Input placeholder="0.000.1111" {...field} />
+                      </FormControl>
+                      <div className="w-full text-sm text-end">
+                        <span className="text-[#9CA3AF]">
+                          Is your Evening Stop the same?{" "}
+                          <a
+                            href="#"
+                            className="bg-transparent text-violetBlue font-bold underline"
+                            onClick={removeEveningStop}
+                          >
+                            Click me
+                          </a>
+                        </span>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                >
-                  <p>{user ? "Save" : "Proceed"}</p>
-                </Button>
-                {/* </Link> */}
+                />
+                <div className="pt-8 w-full">
+                  <Button
+                    type="submit"
+                    className="bg-violetBlue w-full h-10 rounded-lg text-base"
+                  >
+                    Save
+                  </Button>
+                </div>
               </form>
             </Form>
           </div>
         </div>
-        {/* <div className="sticky w-full flex bottom-0 items-center justify-center bg-white min-h-24 z-20"></div> */}
+
+        <div className="w-full flex justify-center">
+          <p className={cn("font-light text-[#979797] text-[12px]")}>
+            Copyright © 2023 All Rights Reserved
+          </p>
+        </div>
       </div>
     </div>
   );
